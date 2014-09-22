8a57da0838641699a1b335a3a02498ab4c5007b5
Vyacheslav Kozyrev <vyacheslav.kozyrev@gmail.com>
2014-09-21 19:31:51 -0500

    login UI

--- src/main/webapp/app/js/const.js
+++ src/main/webapp/app/js/const.js
@@ -1,4 +1,9 @@
 SERVICE_GET_CITY_URL = '/Hackathon/city/listCities.do';
 SERVICE_GET_TRIPS_URL = '/Hackathon/trip/findTrip.do';
+SERVICE_LOGIN_URL = '/Hackathon/login.do';
+
+//SERVICE_GET_CITY_URL = '/app/js/data/city.json';
+//SERVICE_GET_TRIPS_URL = '/app/js/data/trips.json';
+
 SERVICE_GET_TRIPS_LOCAL_URL = '/Hackathon/app/js/data/trips.json';
 SERVICE_PUT_TRIP_URL = '/Hackathon/app/js/data/trip-put.json';
